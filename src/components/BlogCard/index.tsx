67d7a22ba85d6467531dfbcc329894a078cb511f
Calvi <calvi.alejo@gmail.com>
2022-04-27 10:55:13 -0300

    feat: show ubication

--- src/components/BlogCard/index.tsx
+++ src/components/BlogCard/index.tsx
@@ -4,13 +4,13 @@ import type { IBlogCard } from "../types";
 
 import styles from "./index.module.css";
 
-const BlogCard = ({ id, name, description, image, state }: IBlogCard) => {
+const BlogCard = ({ id, name, ubication, description, image, state }: IBlogCard) => {
   return (
     <div className="card" id={`${id}`}>
       <h3>{name}</h3>
-      <p>{state}</p>
+      <p>{ubication}</p>
       <div className={styles.container}>
-        {image ? <img alt="" loading="lazy" src={image} /> : <div>Loading...</div>}
+        {image  ? <img alt="" loading="lazy" src={image}  /> : <div>Loading...</div>}
       </div>
       <button className="btnDescription">{description}</button>
     </div>
