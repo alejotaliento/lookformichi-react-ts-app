287459adc48cb35e695b8ef5acc3bc19349a9c66
Alejo Taliento <alee.taliento@gmail.com>
2022-04-30 13:02:15 -0300

    styles: remove root class BlogCardList

--- src/components/BlogCardList/index.tsx
+++ src/components/BlogCardList/index.tsx
@@ -2,20 +2,14 @@ import React from "react";
 
 import BlogCard from "../BlogCard";
 import type { IBlogCard } from "../types";
-import modalDetalles from "../Modals/modal";
-
-import styles from "./index.module.css";
+//import modalDetalles from "../Modals/modal";
 
 export interface BlogCardListProps {
   data: IBlogCard[];
 }
 
 const BlogCardList = ({ data }: BlogCardListProps) => {
-  return (
-    <div className={styles.root}>
-      {data && data.map((item, index) => <BlogCard key={index} {...item} />)}
-    </div>
-  );
+  return <>{data && data.map((item, index) => <BlogCard key={index} {...item} />)}</>;
 };
 
 export default BlogCardList;
